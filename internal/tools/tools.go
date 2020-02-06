d52fa9b350118a8e5944c54122dcc0f836b9913b
Dmitrij Koniajev <dimchansky@gmail.com>
2020-02-06 18:47:38 +0200

    formatting tools

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -3,7 +3,7 @@
 package tools
 
 import (
-	_ "golang.org/x/lint/golint"                     // tool
-	_ "golang.org/x/tools/cmd/goimports"             // tool
-	_ "honnef.co/go/tools/cmd/staticcheck"           // tool
+	_ "golang.org/x/lint/golint"           // tool
+	_ "golang.org/x/tools/cmd/goimports"   // tool
+	_ "honnef.co/go/tools/cmd/staticcheck" // tool
 )
